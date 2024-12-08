03295d194b7b92fc10eba8f92a27429fca705d63
lrbjk <118360194+lrbjk@users.noreply.github.com>
2024-12-08 18:35:57 +0800

    新的移动

---
+++ SuperMarket_Remake/Assets/RainbowLi/Scripts/BezierPointer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierPointer : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    public Transform controlPoint;
+    public float duration = 1.0f;
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 100; // 增加点的数量来绘制曲线
+    }
+
+    void Update()
+    {
+        DrawBezierCurve();
+        // 可添加其他控制光标的逻辑
+    }
+
+    void DrawBezierCurve()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            float t = i / 100f;
+            Vector3 point = Mathf.Pow(1 - t, 2) * startPoint.position +
+                            2 * (1 - t) * t * controlPoint.position +
+                            Mathf.Pow(t, 2) * endPoint.position;
+            lineRenderer.SetPosition(i, point);
+        }
+    }
+    public Vector3 GetEndPoint()
+    {
+        return endPoint.position;
+    }
+}
---
+++ SuperMarket_Remake/Assets/RainbowLi/Scripts/Transport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport : MonoBehaviour
+{
+    public GameObject player; // 玩家角色
+    public BezierPointer bezierPointer;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) // 用于检测传送的按钮
+        {
+            Vector3 targetPosition = bezierPointer.GetEndPoint(); // 通过光标获取目标位置
+            TeleportPlayer(targetPosition);
+        }
+    }
+
+    private void TeleportPlayer(Vector3 targetPosition)
+    {
+        player.transform.position = new Vector3(targetPosition.x, player.transform.position.y, targetPosition.z); // 传送玩家
+    }
+}
